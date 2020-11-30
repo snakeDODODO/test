ed62e8ebcdf885e2db82d086bb6feb543bc40910
wuxin <43463038+snakeDODODO@users.noreply.github.com>
2020-11-30 15:17:19 +0800

    Create 113002.实现接口.App.java

---
+++ 113002.实现接口.App.java
@@ -0,0 +1,13 @@
+package pac;
+
+public class App implements R{
+	public static void main(String[] args) {
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[i].length; j++) {
+				System.out.println(array2d[i][j]);
+			}
+		}
+	}
+}
