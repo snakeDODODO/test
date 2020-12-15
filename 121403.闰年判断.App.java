3e5688accd80415fd5ea7ad2192b5d0b45101a6f
wuxin <43463038+snakeDODODO@users.noreply.github.com>
2020-12-15 11:39:05 +0800

    Update 121403.闰年判断.App.java

--- 121403.闰年判断.App.java
+++ 121403.闰年判断.App.java
@@ -6,20 +6,25 @@ public class App {
     public static void main(String[] args) {
         System.out.println("请输入年份");
         int year;//接收输入的年份
-        Scanner scanner = new Scanner(System.in);
-        year = scanner.nextInt();
-        if (year < 0) {
-            if (year%4==1&&year%100!=0||year%400==1){
-                System.out.println("公元前" + year + "是闰年");
-            }else {
-                System.out.println("公元前" + year + "不是闰年");
-            }
-        }else if (year > 3200) {
-            if(year%4==0&&year%100!=0||year%400==0){
-                System.out.println("公元" + year + "是闰年");
-            }else {
-                System.out.println("公元" + year + "不是闰年");
-            }
-        }
+        Scanner scan = new Scanner(System.in);
+        year = scan.nextInt();
+        if (year > -3200 && year < 3200) {
+			if (year < 0) {
+	            if (year%4==1||year%400==1){
+	                System.out.println("公元前" + year + "是闰年");
+	            }else {
+	                System.out.println("公元前" + year + "不是闰年");
+	            }
+			}else {
+	            if(year%4==0&&year%100!=0||year%400==0){
+	                System.out.println("公元" + year + "是闰年");
+	            }else {
+	                System.out.println("公元" + year + "不是闰年");
+	            }
+
+			}
+		}else {
+			System.out.println("只能输入-3200到3200之内的数字");
+		}
     }
 }
