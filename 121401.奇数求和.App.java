ecc6273321f4d4a9e68ddc3ec5a9cca8067c20f7
wuxin <43463038+snakeDODODO@users.noreply.github.com>
2020-12-15 11:54:30 +0800

    Update 121401.奇数求和.App.java

--- 121401.奇数求和.App.java
+++ 121401.奇数求和.App.java
@@ -1,1 +1,17 @@
-package pac
+package pac;
+
+
+public class App {
+	public static void main(String[] args) {
+		long i = 1;
+		long sum = 0;
+		while (i <= 2147483647){
+	        if (i % 2 == 1){
+	            sum = sum + i;
+	        }
+	        i++;
+	    }
+	    System.out.println("sum = "+sum);
+	}
+}
+
